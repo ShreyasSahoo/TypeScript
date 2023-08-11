8144e0b314f2b92b76742d67c91bba7045da13ba
Shreyas Sahoo <shreyassahoo26@gmail.com>
2023-08-12 00:05:34 +0530

    everyday types added

---
+++ Everyday-Types/build/everyday-types.js
@@ -0,0 +1,228 @@
+"use strict";
+//The primitive Types: string, number, and boolean
+//for arrays --> Array<type> or type[]
+//any - TypeScript also has a special type, any, that you can use whenever you don’t want a particular value to cause typechecking errors.
+let obj = { x: 0 };
+// None of the following lines of code will throw compiler errors.
+// Using `any` disables all further type checking, and it is assumed
+// you know the environment better than TypeScript.
+obj.foo();
+obj();
+obj.bar = 100;
+obj = "hello";
+const n = obj;
+//The any type is useful when you don’t want to write out a long type just to convince TypeScript that a particular line of code is okay.
+// When you don’t specify a type, and TypeScript can’t infer it from context, the compiler will typically default to any.
+// You usually want to avoid this, though, because any isn’t type-checked. Use the compiler flag noImplicitAny to flag any implicit any as an error.
+// When you declare a variable using const, var, or let, you can optionally add a type annotation to explicitly specify the type of the variable:
+let firstName = "Shreyas";
+// In most cases, though, this isn’t needed. Wherever possible, TypeScript tries to automatically infer the types in your code.
+let lastName = "Sahoo";
+//Parameter Type Annotations
+// When you declare a function, you can add type annotations after each parameter to declare what types of parameters the function accepts. Parameter type annotations go after the parameter name:
+function greet(name) {
+    console.log(`Helllo ${name.toUpperCase()}`);
+}
+//Even if you don’t have type annotations on your parameters, TypeScript will still check that you passed the right number of arguments.
+//You can also add return type annotations. Return type annotations appear after the parameter list:
+function getFavoriteNumber() {
+    return 26;
+}
+// Anonymous functions are a little bit different from function declarations. When a function appears in a place where TypeScript can determine how it’s going to be called, the parameters of that function are automatically given types
+const names = ["Alice", "Bob", "Eve"];
+// Contextual typing for function - parameter s inferred to have type string
+names.forEach(function (s) {
+    console.log(s.toUpperCase());
+});
+// Contextual typing also applies to arrow functions
+names.forEach((s) => {
+    console.log(s.toUpperCase());
+});
+// This process is called contextual typing because the context that the function occurred within informs what type it should have.
+//Object Types
+//To define an object type, we simply list its properties and their types.
+// The parameter's type annotation is an object type
+function printCoord(pt) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 3, y: 7 });
+//The type part of each property is also optional. If you don’t specify a type, it will be assumed to be any.
+// Optional Properties
+// Object types can also specify that some or all of their properties are optional. To do this, add a ? after the property name:
+function printName(obj) {
+    // ...
+}
+// Both OK
+printName({ first: "Bob" });
+printName({ first: "Alice", last: "Alisson" });
+//In JavaScript, if you access a property that doesn’t exist, you’ll get the value undefined rather than a runtime error. Because of this, when you read from an optional property, you’ll have to check for undefined before using it.
+function printName2(obj) {
+    var _a;
+    // Error - might crash if 'obj.last' wasn't provided!
+    // console.log(obj.last.toUpperCase());
+    //   'obj.last' is possibly 'undefined'.
+    if (obj.last !== undefined) {
+        // OK
+        console.log(obj.last.toUpperCase());
+    }
+    // A safe alternative using modern JavaScript syntax:
+    console.log((_a = obj.last) === null || _a === void 0 ? void 0 : _a.toUpperCase());
+}
+//Union Types
+//The first way to combine types you might see is a union type. A union type is a type formed from two or more other types, representing values that may be any one of those types. We refer to each of these types as the union’s members.
+function printId(id) {
+    console.log("Your ID is: " + id);
+}
+// OK
+printId(101);
+// OK
+printId("202");
+// Error
+//   printId({ myID: 22342 });
+// TypeScript will only allow an operation if it is valid for every member of the union. For example, if you have the union string | number, you can’t use methods that are only available on string:
+// The solution is to narrow the union with code, the same as you would in JavaScript without type annotations. Narrowing occurs when TypeScript can deduce a more specific type for a value based on the structure of the code.
+// For example, TypeScript knows that only a string value will have a typeof value "string":
+function printId2(id) {
+    if (typeof id === "string") {
+        // In this branch, id is of type 'string'
+        console.log(id.toUpperCase());
+    }
+    else {
+        // Here, id is of type 'number'
+        console.log(id);
+    }
+}
+// Another example is to use a function like Array.isArray:
+function welcomePeople(x) {
+    if (Array.isArray(x)) {
+        // Here: 'x' is 'string[]'
+        console.log("Hello, " + x.join(" and "));
+    }
+    else {
+        // Here: 'x' is 'string'
+        console.log("Welcome lone traveler " + x);
+    }
+}
+// Notice that in the else branch, we don’t need to do anything special - if x wasn’t a string[], then it must have been a string.
+// Sometimes you’ll have a union where all the members have something in common. For example, both arrays and strings have a slice method. If every member in a union has a property in common, you can use that property without narrowing:
+// Return type is inferred as number[] | string
+function getFirstThree(x) {
+    return x.slice(0, 3);
+}
+// Exactly the same as the earlier example
+function printCoord2(pt) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 100, y: 100 });
+function printCoord3(pt) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 100, y: 100 });
+// Differences Between Type Aliases and Interfaces
+// Type aliases and interfaces are very similar, and in many cases you can choose between them freely. Almost all features of an interface are available in type, the key distinction is that a type cannot be re-opened to add new properties vs an interface which is always extendable.
+// Interface	Type
+// Extending an interface
+// interface Animal {
+//   name: string;
+// }
+// interface Bear extends Animal {
+//   honey: boolean;
+// }
+// const bear = getBear();
+// bear.name;
+// bear.honey;
+// Extending a type via intersections
+// type Animal = {
+//   name: string;
+// }
+// type Bear = Animal & {
+//   honey: boolean;
+// }
+// const bear = getBear();
+// bear.name;
+// bear.honey;
+// Adding new fields to an existing interface
+// interface Window {
+//   title: string;
+// }
+// interface Window {
+//   ts: TypeScriptAPI;
+// }
+// const src = 'const a = "Hello World"';
+// window.ts.transpileModule(src, {});
+// A type cannot be changed after being created
+// type Window = {
+//   title: string;
+// }
+// type Window = {
+//   ts: TypeScriptAPI;
+// }
+// Error: Duplicate identifier 'Window'.
+//  Type Assertions
+//  Sometimes you will have information about the type of a value that TypeScript can’t know about.
+//  For example, if you’re using document.getElementById, TypeScript only knows that this will return some kind of HTMLElement, but you might know that your page will always have an HTMLCanvasElement with a given ID.
+//  In this situation, you can use a type assertion to specify a more specific type:
+const myCanvas = document.getElementById("main_canvas");
+// TypeScript only allows type assertions which convert to a more specific or less specific version of a type. This rule prevents “impossible” coercions like:
+//  const x = "hello" as number;
+// Sometimes this rule can be too conservative and will disallow more complex coercions that might be valid. If this happens, you can use two assertions, first to any (or unknown, which we’ll introduce later), then to the desired type:
+// const a = (expr as any) as T;
+// It’s not much use to have a variable that can only have one value!
+// But by combining literals into unions, you can express a much more useful concept - for example, functions that only accept a certain set of known values:
+//Literal Types
+function printText(s, alignment) {
+    // ...
+}
+printText("Hello, world", "left");
+printText("G'day, mate", "center");
+function configure(x) {
+    // ...
+}
+configure({ width: 100 });
+configure("auto");
+// configure("automatic");
+// Literal Inference
+// When you initialize a variable with an object, TypeScript assumes that the properties of that object might change values later. For example, if you wrote code like this:
+const obj = { counter: 0 };
+if (someCondition) {
+    obj.counter = 1;
+}
+const req = { url: "https://example.com", method: "GET" };
+// handleRequest(req.url, req.method);
+//Argument of type 'string' is not assignable to parameter of type '"GET" | "POST"'.
+// In the above example req.method is inferred to be string, not "GET". Because code can be evaluated between the creation of req and the call of handleRequest which could assign a new string like "GUESS" to req.method, TypeScript considers this code to have an error.
+// There are two ways to work around this.
+// You can change the inference by adding a type assertion in either location:
+// Change 1:
+const req = { url: "https://example.com", method: "GET" };
+// Change 2
+handleRequest(req.url, req.method);
+// Change 1 means “I intend for req.method to always have the literal type "GET"”, preventing the possible assignment of "GUESS" to that field after. Change 2 means “I know for other reasons that req.method has the value "GET"“.
+// You can use as const to convert the entire object to be type literals:
+const req = { url: "https://example.com", method: "GET" };
+handleRequest(req.url, req.method);
+// The as const suffix acts like const but for the type system, ensuring that all properties are assigned the literal type instead of a more general version like string or number.
+// null and  undefined
+// JavaScript has two primitive values used to signal absent or uninitialized value: null and undefined.
+// TypeScript has two corresponding types by the same names. How these types behave depends on whether you have the strictNullChecks option on.
+// strictNullChecks off
+// With strictNullChecks off, values that might be null or undefined can still be accessed normally, and the values null and undefined can be assigned to a property of any type. This is similar to how languages without null checks (e.g. C#, Java) behave. The lack of checking for these values tends to be a major source of bugs; we always recommend people turn strictNullChecks on if it’s practical to do so in their codebase.
+// strictNullChecks on
+// With strictNullChecks on, when a value is null or undefined, you will need to test for those values before using methods or properties on that value. Just like checking for undefined before using an optional property, we can use narrowing to check for values that might be null:
+function doSomething(x) {
+    if (x === null) {
+        // do nothing
+    }
+    else {
+        console.log("Hello, " + x.toUpperCase());
+    }
+}
+//   TypeScript also has a special syntax for removing null and undefined from a type without doing any explicit checking. Writing ! after any expression is effectively a type assertion that the value isn’t null or undefined:
+function liveDangerously(x) {
+    // No error
+    console.log(x.toFixed());
+}
+//Just like other type assertions, this doesn’t change the runtime behavior of your code, so it’s important to only use ! when you know that the value can’t be null or undefined.
--- The Basics/build/basics.js
+++ The Basics/build/basics.js
@@ -1,1 +1,5 @@
 "use strict";
+function greet(person, date) {
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+//Most TypeScript-specific code gets erased away, and likewise, here our type annotations were completely erased.
